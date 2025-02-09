c5576eb8137043c3cf0558bcf9c821005da09341
NoUhMaybe <nouhmaybe@outlook.com>
2025-02-08 18:15:56 -0600

    Changed the interfac to an abstract class

--- CookingRecipes/Ingredients/Ingredients.cs
+++ CookingRecipes/Ingredients/Ingredients.cs
@@ -1,67 +1,65 @@
 ﻿
-interface Ingredients
+public abstract class Ingredients
 {
-    int ID();
-    string Name();
-    string PreperationInstructions();
-
-
+    public abstract int ID();
+    public abstract string Name();
+    public abstract string PreperationInstructions();
 }
 
 public class WheatFlour : Ingredients
 {
-    public int ID() => 1;
-    public string Name() => "Wheat Flour";
-    public string PreperationInstructions() => "Sieve. Add to other ingredients.";
+    public override int ID() => 1;
+    public override string Name() => "Wheat Flour";
+    public override string PreperationInstructions() => "Sieve. Add to other ingredients.";
 }
 
 public class CoconutFlour : Ingredients
 {
-    public int ID() => 2;
-    public string Name() => "Coconut Flour";
-    public string PreperationInstructions() => "Sieve. Add to other ingredients.";
+    public override int ID() => 2;
+    public override string Name() => "Coconut Flour";
+    public override string PreperationInstructions() => "Sieve. Add to other ingredients.";
 }
 
 public class Butter : Ingredients
 {
-    public int ID() => 3;
-    public string Name() => "Butter";
-    public string PreperationInstructions() => "Melt on low heat. Add to other ingredients.";
+    public override int ID() => 3;
+    public override string Name() => "Butter";
+    public override string PreperationInstructions() => "Melt on low heat. Add to other ingredients.";
 }
 
 public class Chocolate : Ingredients
 {
-    public int ID() => 4;
-    public string Name() => "Chocolate";
-    public string PreperationInstructions() => "Melt on low heat. Add to other ingredients.";
+    public override int ID() => 4;
+    public override string Name() => "Chocolate";
+    public override string PreperationInstructions() => "Melt on low heat. Add to other ingredients.";
 }
 
 public class Sugar : Ingredients
 {
-    public int ID() => 5;
-    public string Name() => "Sugar";
-    public string PreperationInstructions() => "Add to other ingredients.";
+    public override int ID() => 5;
+    public override string Name() => "Sugar";
+    public override string PreperationInstructions() => "Add to other ingredients.";
 }
 
 public class Cardamom : Ingredients
 {
-    public int ID() => 6;
-    public string Name() => "Caramom";
-    public string PreperationInstructions() => "Take half a teaspoon. Add to other ingredients.";
+    public override int ID() => 6;
+    public override string Name() => "Caramom";
+    public override string PreperationInstructions() => "Take half a teaspoon. Add to other ingredients.";
 }
 
 public class Cinnamon : Ingredients
 {
-    public int ID() => 7;
-    public string Name() => "Cinnamon";
-    public string PreperationInstructions() => "Take half a teaspoon. Add to other ingredients.";
+    public override int ID() => 7;
+    public override string Name() => "Cinnamon";
+    public override string PreperationInstructions() => "Take half a teaspoon. Add to other ingredients.";
 }
 
 public class CocoaPowder : Ingredients
 {
-    public int ID() => 8;
-    public string Name() => "Cocoa Powder";
-    public string PreperationInstructions() => "Add to other ingredients.";
+    public override int ID() => 8;
+    public override string Name() => "Cocoa Powder";
+    public override string PreperationInstructions() => "Add to other ingredients.";
 }
 
 
