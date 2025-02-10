70b5272ff47090ad38a1d5966215ba36464d57c9
NoUhMaybe <nouhmaybe@outlook.com>
2025-02-10 13:49:41 -0600

    Fixed organization, made a recipe file class for I/O and processing of recipes

--- CookingRecipes/Program.cs
+++ CookingRecipes/Program.cs
@@ -1,21 +1,37 @@
 ﻿using System;
+using System.Collections.Immutable;
 using System.IO;
 
-const string FilePath = "recipes";
-const string FileType = ".json";
+var recipeList = new RecipeFile();
 
+//Add a new recipe
+List<Ingredients> recipe = Recipe.Add();
 
-var recipe = Recipe.Add();
+//Fixme: Move the following function to a class file
+//Output the recipe with concatenation
+string output = "";
+for (int i = 0; i < recipe.Count; i++)
+{
+    if (i == 0)//If beginning iteration of the loop
+    {
+        output += $"[\"{recipe[i].ID()}";
+    }
+    else if (i == recipe.Count - 1) //if on last iteration of loop
+    {
+        output += $",{recipe[i].ID()}\"]";
+    }
+    else //If anywhere in the middle iterations
+    {
+        output += $",{recipe[i].ID()}";
+    }
+}
 
-//Check if a recipes file already exists, if not, make it
-//Write recipe to specified file
+//Testing output, to be commented out later
+Console.WriteLine(output);
 
+//FIXME: Figure out how to write 'output' to a file
 
-
-
-
-
+//Exit
+Console.WriteLine("Press any key to exit...");
 Console.ReadKey();
-
-
 
---
+++ CookingRecipes/RecipeFile/RecipeFile.cs
@@ -0,0 +1,53 @@
+﻿
+public class RecipeFile
+{
+    private string _filename = "";
+    public void InputFile(List<Ingredients> recipe)
+    {
+        //Initial variable declaration
+        string userChoice = "";
+
+        //Prompt user for a filename/type
+        do
+        {
+            Console.WriteLine("Enter a filename for the recipe (Ex recipes): ");
+            _filename = Console.ReadLine();
+            if(_filename.Length == 0) { continue; }
+            Console.WriteLine("Would you like it to be a [1].txt or [2].json: ");
+            userChoice = Console.ReadLine();
+
+            switch (userChoice)
+            {
+                case "1":
+                    _filename += ".txt";
+                    break;
+                case "2":
+                    _filename += ".json";
+                    break;
+                default:
+                    break;
+            }
+        } while (userChoice != "1" || userChoice != "2");
+
+        //Check if file already exists
+        if (CheckFile())
+        {
+            this.OutputRecipe();
+            //Fixme: ask user if they want to overwrite
+            Console.WriteLine("Would you like to Overwrite (y/n): ");
+            var userChoice = Console.ReadLine();
+            //Fixme: either append or delete and start appending file depending on users choice to overwrite
+        }
+    }
+
+    public void OutputRecipe()
+    {
+        //Fixme: Display current recipes written in file
+    }
+
+    private bool CheckFile()
+    {
+        if (File.Exists(_filename)) {return true;}
+        return false;
+    }
+}
